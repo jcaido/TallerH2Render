4bc377bedac13a52bc4609bbe8ebee5188ea00f4
Cristobal Rosa <crisrogar@gmail.com>
2024-04-21 19:12:28 +0200

    create model CodigoPostal

---
+++ src/main/java/com/jcaido/TallerH2Render/models/CodigoPostal.java
@@ -0,0 +1,25 @@
+package com.jcaido.TallerH2Render.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "codigos_postales")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class CodigoPostal implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "codigo_postal", unique = true)
+    private String codigo;
+    @Column(unique = true)
+    private String localidad;
+    private String provincia;
+}
