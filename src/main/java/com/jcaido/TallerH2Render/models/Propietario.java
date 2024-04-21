32dadad9511e4a873d02664b5ca7457958db2721
Cristobal Rosa <crisrogar@gmail.com>
2024-04-21 19:32:45 +0200

    create model Propietario

---
+++ src/main/java/com/jcaido/TallerH2Render/models/Propietario.java
@@ -0,0 +1,28 @@
+package com.jcaido.TallerH2Render.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "propietarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Propietario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    @Column(name = "primer_apellido")
+    private String primerApellido;
+    @Column(name = "segundo_apellido")
+    private String segundoApellido;
+    @Column(unique = true)
+    private String dni;
+    private String domicilio;
+}
