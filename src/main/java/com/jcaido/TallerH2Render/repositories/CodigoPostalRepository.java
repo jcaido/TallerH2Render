43aa8224799ebc8001e3b5fe36a02a74c27b6189
Cristobal Rosa <crisrogar@gmail.com>
2024-04-22 18:28:00 +0200

    update reposistory CodigoPostalRepository

--- src/main/java/com/jcaido/TallerH2Render/repositories/CodigoPostalRepository.java
+++ src/main/java/com/jcaido/TallerH2Render/repositories/CodigoPostalRepository.java
@@ -3,7 +3,21 @@ package com.jcaido.TallerH2Render.repositories;
 import com.jcaido.TallerH2Render.models.CodigoPostal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CodigoPostalRepository extends JpaRepository<CodigoPostal, Long> {
+    @Transactional(readOnly = true)
+    Optional<CodigoPostal> findByCodigo(String codigo);
+    @Transactional(readOnly = true)
+    List<CodigoPostal> findByProvincia (String provincia);
+    @Transactional(readOnly = true)
+    Optional<CodigoPostal> findByLocalidad(String Localidad);
+    @Transactional(readOnly = true)
+    boolean existsByCodigo(String codigo);
+    @Transactional(readOnly = true)
+    boolean existsByLocalidad(String localidad);
 }
