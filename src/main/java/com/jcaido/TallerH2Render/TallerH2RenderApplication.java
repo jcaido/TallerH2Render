5b83a9d138ca459edf6dd5d031b61244b2b52805
Cristobal Rosa <crisrogar@gmail.com>
2024-04-23 11:02:44 +0200

    create Bean PropietarioDTO

--- src/main/java/com/jcaido/TallerH2Render/TallerH2RenderApplication.java
+++ src/main/java/com/jcaido/TallerH2Render/TallerH2RenderApplication.java
@@ -1,6 +1,7 @@
 package com.jcaido.TallerH2Render;
 
 import com.jcaido.TallerH2Render.DTOs.codigoPostal.CodigoPostalDTO;
+import com.jcaido.TallerH2Render.DTOs.propietario.PropietarioDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +14,14 @@ public class TallerH2RenderApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-
 	@Bean
 	public CodigoPostalDTO codigoPostalDTO() {
 		return new CodigoPostalDTO();
+	}
+
+	@Bean
+	public PropietarioDTO propietarioDTO() {
+		return new PropietarioDTO();
 	}
 
 	public static void main(String[] args) {
