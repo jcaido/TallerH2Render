56954c4858f907037f0b6724ec3c51e78c49e26a
Cristobal Rosa <crisrogar@gmail.com>
2024-04-25 18:57:29 +0200

    crear DTOs Pieza

---
+++ src/main/java/com/jcaido/TallerH2Render/DTOs/pieza/PiezaDTO.java
@@ -0,0 +1,20 @@
+package com.jcaido.TallerH2Render.DTOs.pieza;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PiezaDTO {
+    private Long id;
+    @NotBlank(message =  "debe introducir la refenciare")
+    private String referencia;
+    @NotBlank(message =  "debe introducir el nombre")
+    private String nombre;
+    private Double precio;
+}
