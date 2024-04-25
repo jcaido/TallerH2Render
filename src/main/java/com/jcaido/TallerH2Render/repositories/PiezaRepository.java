1058cfb9da64b4b870688460864067f5c94dc954
Cristobal Rosa <crisrogar@gmail.com>
2024-04-25 18:53:29 +0200

    create Repository PiezaRepository

---
+++ src/main/java/com/jcaido/TallerH2Render/repositories/PiezaRepository.java
@@ -0,0 +1,23 @@
+package com.jcaido.TallerH2Render.repositories;
+
+import com.jcaido.TallerH2Render.models.Pieza;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PiezaRepository extends JpaRepository<Pieza, Long> {
+    @Transactional(readOnly = true)
+    Optional<Pieza> findByReferencia(String referencia);
+    @Transactional(readOnly = true)
+    List<Pieza> findByNombre (String nombre);
+    @Transactional(readOnly = true)
+    boolean existsByReferencia(String referencia);
+    @Transactional(readOnly = true)
+    boolean existsByNombre(String nombre);
+    @Transactional(readOnly = true)
+    boolean existsById(Long id);
+}
