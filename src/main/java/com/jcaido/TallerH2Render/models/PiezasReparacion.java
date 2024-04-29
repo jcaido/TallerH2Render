a1a6eb891f87f9295e2acd5e7f0f0e1cc68316a2
Cristobal Rosa <crisrogar@gmail.com>
2024-04-29 18:27:52 +0200

    create model PiezasReparacion

---
+++ src/main/java/com/jcaido/TallerH2Render/models/PiezasReparacion.java
@@ -0,0 +1,26 @@
+package com.jcaido.TallerH2Render.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+@Entity
+@Table(name = "piezas_reparacion")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class PiezasReparacion implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne()
+    @JoinColumn(name = "pieza_id")
+    private Pieza pieza;
+    private Integer cantidad;
+
+}
