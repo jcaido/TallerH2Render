414fb5d09555b6176122b23d6c94b847d246c7d9
Cristobal Rosa <crisrogar@gmail.com>
2024-05-06 13:30:58 +0200

    create DTOs AlbaranProveedor

---
+++ src/main/java/com/jcaido/TallerH2Render/DTOs/albaranProveedor/AlbaranProveedorBusquedasParcialDTO.java
@@ -0,0 +1,22 @@
+package com.jcaido.TallerH2Render.DTOs.albaranProveedor;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AlbaranProveedorBusquedasParcialDTO {
+    private Long id;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate fechaAlbaran;
+    private String numeroAlbaran;
+    private String proveedorNombre;
+    private String proveedorDniCif;
+}
